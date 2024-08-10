b5f0ef565e79aa10c54f0538c45aefdd2d471413
Dafydd <dafjwork@gmail.com>
2024-08-10 09:54:21 +0100

    Initial Commit

---
+++ first app/first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_appApp.swift
+//  first app
+//
+//  Created by Daf James on 10/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
