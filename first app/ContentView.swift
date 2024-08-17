d5f25e19e5196db99cee660f8fd03e2c01dbaa1d
Dafydd <dafjwork@gmail.com>
2024-08-17 09:49:33 +0100

    fix

--- first app/ContentView.swift
+++ first app/ContentView.swift
@@ -1,9 +1,9 @@
+
 //
 //  ContentView.swift
 //  first app
 //
 //  Created by Daf James on 10/08/2024.
-//
 
 import SwiftUI
 
@@ -16,6 +16,20 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        VStack(alignment: .leading) {
+            Text("turtle rock")
+                .font(.title)
+                .foregroundColor(Color.orange)
+                .bold()
+            .foregroundStyle(.purple)
+            HStack {
+                Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+                    .font(.subheadline)
+                .fontWeight(.light)
+                Text("California")
+            }
+        }
+
     }
 }
 
